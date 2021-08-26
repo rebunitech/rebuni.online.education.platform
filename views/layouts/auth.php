a5ce071a1ed991dc1e4ae0ba9faf89215065a339
wendirad <wendiradame@gmail.com>
2021-08-26 10:24:09 +0300

    [feat] Add template to auth layout

--- views/layouts/auth.php
+++ views/layouts/auth.php
@@ -1,10 +1,43 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Auth</title>
-</head>
-<body>
-	{{content}}
-</body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.87.0">
+    <title>BOEP</title>
+
+      
+    <!-- Bootstrap core CSS -->
+	<link href="/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="/assets/css/auth.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    
+	<main class="form-signin">
+	  {{content}}
+	</main>
+	    
+  </body>
 </html>
+
--- views/register.php
+++ views/register.php
@@ -1,8 +1,8 @@
 
 <h1>Register</h1>
-<?php use app\core\form\Field; ?>
+<?php use app\core\form\Field; use app\core\form\Form; ?>
 
-<?php $form = app\core\form\Form::begin("", "post"); ?>
+<?php $form = Form::begin("", "post"); ?>
 	<?php echo $form->field($model, 'first_name'); ?>
 	<?php echo $form->field($model, 'last_name'); ?>
 	<?php echo $form->field($model, 'email'); ?>
@@ -10,30 +10,4 @@
 	<?php echo $form->field($model, 'password', Field::TYPE_PASSWORD); ?>
 	<?php echo $form->field($model, 'passwordConfirm', Field::TYPE_PASSWORD); ?>
 	<button type="submit">Register</button>
-<?php echo app\core\form\Form::end(); ?>
-
-<form action="" method="post">
-<!-- 	<ul>
-		<li>
-			first name <input type="text" class="<?php echo $model->hasError('firstname') ? 'invalid' : '' ?>" name="firstname">
-			<div>
-				<?php echo $model->getFirstError('firstname'); ?>
-			</div>
-		</li>
-		<li>
-			last name <input type="text" name="lastname">
-		</li>
-		<li>
-			email <input type="email" name="email">
-		</li>
-		<li>
-			Password: <input type="password" name="password">
-		</li>
-		<li>
-			Password Confirm: <input type="password" name="passwordConfirm">
-		</li>
-		<li>
-			<button type="submit">Register</button>
-		</li>
-	</ul>
-</form> -->
+<?php echo Form::end(); ?>
